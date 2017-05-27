7c2f7c65bff888b61ebd975d8e7f5a6208c3e005
Jean-Marc Amon <jmbdilem@gmail.com>
2017-05-27 00:21:55 +0000

    add .env

--- bootstrap/settings.php
+++ bootstrap/settings.php
@@ -1,26 +1,25 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: marcamon
+ * User: donmarkus
  * Date: 07/10/2016
  * Time: 16:12
  */
 
 return [
     'settings' => [
-        'displayErrorDetails' => true,
+        'displayErrorDetails' => (bool)getenv('DISPLAY_ERRORS'),
         'db' => [
-            'driver' => 'mysql',
-            'database' => 'slim3_starter',
-            'host' => 'localhost',
-            'username' => 'homestead',
-            'password' => 'secret',
+            'driver' => getenv('DB_DRIVER'),
+            'database' => getenv('DB_DATABASE'),
+            'host' => getenv('DDB_HOST'),
+            'username' => getenv('DB_USERNAME'),
+            'password' => getenv('DB_PASSWORD'),
             'collation' => 'utf8_unicode_ci',
             'charset' => 'utf8',
             'prefix' => ''
         ],
-        'debug' => true,
+        'debug' => (bool)getenv('APP_DEBUG'),
         'whoops.editor' => 'phpstorm' // Support click to open editor
     ],
-
 ];
